0da83dfd7e9f597e9795fb119c10118813be975d
DominoSl <artemenko.mik@gmail.com>
2023-01-04 21:21:28 +0200

    feat: problem solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(number) {
+  const isNegative = number < 0;
+  let numberAsString = number + '';
+  let reversedNumber = '';
+
+  if (isNegative) {
+    numberAsString = numberAsString.slice(1); 
+  }
+
+  for (const char of numberAsString) {
+    reversedNumber = char + reversedNumber;
+  }
+
+  return +reversedNumber;
 }
